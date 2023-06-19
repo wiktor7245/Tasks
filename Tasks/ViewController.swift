00d448590d61637a850434f0758eb674a1f05021
Wiktor Tylman <wiktor.tylman@rst.com.pl>
2023-06-19 20:28:58 +0200

    Initial Commit

---
+++ Tasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tasks
+//
+//  Created by Wiktor Tylman on 19/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
